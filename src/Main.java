a6a187a30440fae022990a172224c59ef7465da7
Пользователь <d.elllka@bk.ru>
2023-06-01 14:58:34 +0300

    hw

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import exeptions.WrongLoginException;
+import exeptions.WrongPasswordException;
+
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+          parameters("java_skypro_go", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
+        } catch (WrongPasswordException | WrongLoginException e) {
+            System.out.println(e.getClass());
+        }
     }
+
+    private static void parameters(String javaSkyproGo, String D1hWiKjjP9, String D1hWiKjjP91) {
+    }
+
+
 }
---
+++ src/exeptions/WrongPasswordException.java
@@ -0,0 +1,12 @@
+package exeptions;
+
+public class WrongPasswordException extends RuntimeException{
+
+    public WrongPasswordException(String message) {
+        super(message);
+    }
+
+    public WrongPasswordException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
